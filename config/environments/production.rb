8964b56238418ec4e07acf760ab3b6b5bde50486
Patrick Baselier <patrick.baselier@gmail.com>
2020-05-24 19:13:15 +0200

    Update action_mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,9 +61,11 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "pele_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
+    password: ENV['SENDGRID_API_KEY'],
     domain: 'gmail.com',
     address: 'smtp.sendgrid.net',
     port: 587,
