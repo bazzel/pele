adfabd5deba8f4c2f70c8a89c1fb7c71245302e0
Patrick Baselier <patrick.baselier@gmail.com>
2020-06-03 22:38:36 +0200

    Improve seed

--- spec/factories/scores.rb
+++ spec/factories/scores.rb
@@ -3,5 +3,11 @@
 FactoryBot.define do
   factory :score do
     song { nil }
+    attachment do
+      Rack::Test::UploadedFile.new(
+        Rails.root.join('spec/files/score.pdf'),
+        'application/pdf'
+      )
+    end
   end
 end
--- spec/factories/songs.rb
+++ spec/factories/songs.rb
@@ -2,6 +2,12 @@
 
 FactoryBot.define do
   factory :song do
+    transient { scores_count { rand(6) } }
+
     title { Faker::Music::PearlJam.song }
+
+    after(:create) do |song, evaluator|
+      create_list(:score, evaluator.scores_count, song: song)
+    end
   end
 end
