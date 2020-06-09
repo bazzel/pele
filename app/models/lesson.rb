01477b40c800357513eb45e355bcbe746d338255
Patrick Baselier <patrick.baselier@gmail.com>
2020-06-09 12:06:29 +0200

    Fix ransack_alias

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -9,11 +9,9 @@ class Lesson < ApplicationRecord
   ransack_alias :title, :song_title
   ransack_alias :title_or_songwriter_name, :song_title_or_song_songwriter_name
   ransack_alias :tags_id, :song_tags_id
-  ransack_alias :scores_tablature_notation_true,
-                :song_scores_tablature_notation_true
-  ransack_alias :scores_standard_notation_true,
-                :song_scores_standard_notation_true
-  ransack_alias :scores_chord_notation_true, :song_scores_chord_notation_true
+  ransack_alias :scores_tablature_notation, :song_scores_tablature_notation
+  ransack_alias :scores_standard_notation, :song_scores_standard_notation
+  ransack_alias :scores_chord_notation, :song_scores_chord_notation
 
   scope :pinned_by, ->(user) { merge(user.pins) }
 
